91751820a499e547a37698c4e3abeb2bec786c29
Maksim Murygin <muriginm@gmail.com>
2016-07-10 20:24:58 +0700

    add char line script

---
+++ bin/char_line.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Prints the line of characters with specific length
+# The default char is "-"
+
+length="$1"
+if [[ ! $length ]]; then
+    echo "You should provide the length of line" >&2
+    exit 1
+fi
+
+char="-"
+if [[ $2 ]]; then
+    char="$2"
+fi
+
+line=""
+for (( i = 0; i < $length; i++ )); do
+    line="${line}${char}"
+done
+
+echo $line
+
+exit 0
