c962ba7c9752e1e101fe78f9828475ccf61c1ce0
Maksim Murygin <muriginm@gmail.com>
2016-07-17 14:26:49 +0700

    modify guess game to use arithmetic expressions

--- bin/guess_game.sh
+++ bin/guess_game.sh
@@ -1,20 +1,23 @@
 #!/bin/bash
 
-hidden_number=$(($RANDOM % 100))
+declare -ri target=$(( ($RANDOM % 100) + 1))
 
 echo "Start game"
 
-guess=
+declare -i guess=0
 
-until [[ $guess -eq $hidden_number ]]; do
-    read -r -p "Try to guess number between 0 and 99: " guess
+until [[ $guess -eq $target ]]; do
+    read -r -p "Try to guess number between 1 and 100: " guess
 
-    if [[ $guess -lt $hidden_number ]]; then
-        echo "Your value is LESS than hidden number"
-    elif [[ $guess -gt $hidden_number ]]; then
-        echo "Your value is GREAT then hidden number"
+    # if guess 0, that we have not an integer value
+    ((guess)) || continue
+
+    if (( guess < target )); then
+        echo "Higher!"
+    elif (( guess > target )); then
+        echo "Lower!"
     fi
 done
 
-echo "You guessed. The hidden number is $hidden_number"
+echo "You guessed. The hidden number is $target"
 exit 0
